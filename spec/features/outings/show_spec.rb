fbcf7d8ed285bb19a6765f1b760b6076d478f4bb
Travis McKinstry <mckinstrytg@gmail.com>
2020-09-18 08:02:58 -0600

    Create test for user story 4

--- spec/features/outings/show_spec.rb
+++ spec/features/outings/show_spec.rb
@@ -7,18 +7,24 @@ RSpec.describe "Outing show page" do
 
         bachelorette_1 = Bachelorette.create!(name: "The Spicy Spring", season_number: 3, description: "The most spicy season ever!")
 
+
         contestant_1 = bachelorette_1.contestants.create!(name: "George Henry", age: 32, hometown: "Denver")
         contestant_2 = bachelorette_1.contestants.create!(name: "Travis Nopers", age: 29, hometown: "Denver")
 
-        outing_1 = contestant_1.outings.create!(name: "Kickball", location: "Utah", date: "2-14-20")
-        outing_2 = contestant_1.outings.create!(name: "Hot Springs", location: "New York", date: "3-5-20")
-        outing_3 = contestant_1.outings.create!(name: "Helicopter Ride", location: "New York", date: "3-10-20")
+        outing_1 = Outing.create!(name: "Kickball", location: "Utah", date: "2-14-20")
+        outing_2 = Outing.create!(name: "Sky-diving", location: "New York", date: "5-10-20")
+
+        ContestantOuting.create!(contestant: contestant_1, outing: outing_1)
+        ContestantOuting.create!(contestant: contestant_2, outing: outing_1)
 
         visit "/outing/#{outing_1.id}"
 
         expect(page).to have_content(outing_1.name)
         expect(page).to have_content(outing_1.location)
         expect(page).to have_content(outing_1.date)
+
+        expect(page).to have_content(contestant_1.name)
+        expect(page).to have_content(contestant_2.name)
         end
       end
     end
